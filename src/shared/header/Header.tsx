a40d07ee32a90b7c2e396e26d0fca2a5cc4c8796
Nazar <ups14q@gmail.com>
2024-10-30 22:34:12 +0200

    first commit

---
+++ src/shared/header/Header.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link'
+
+import Input from '@/ui/Input'
+
+import s from './Header.module.scss'
+
+const menuItems = [{ title: 'Home' }, { title: 'Blog' }, { title: 'Shop' }]
+
+const Header = () => {
+  return (
+    <div className={s.main_header}>
+      <div className={s.container}>
+        <div className={s.header_top}>
+          <div className={s.header_top__logo}>
+            <span>Storefront</span>
+          </div>
+          <div className={s.header_top__right_menu}>
+            <div className={s.my_account}>
+              <Link href="/">My Account</Link>
+            </div>
+            <div className={s.search}>
+              <Input placeholder="Search for products" type="text" />
+            </div>
+          </div>
+        </div>
+        <div className={s.header_bottom}>
+          <div className={s.header_bottom__inner}>
+            <ul className={s.header_bottom__menu}>
+              {menuItems.map((i) => {
+                return (
+                  <li>
+                    <Link href="/">{i.title}</Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+          <a href="#" className={s.header_bottom__mini_cart}>
+            <span>0.00</span>
+            <span className={s.header_bottom__item_counter}>0 items</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/ui/Input.tsx
@@ -0,0 +1,13 @@
+import { FC, InputHTMLAttributes } from 'react'
+
+import clsx from 'clsx'
+
+import s from './Input.module.scss'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+const Input: FC<InputProps> = ({ ...props }) => {
+  return <input {...props} className={clsx(s.base_input, props.className)} />
+}
+
+export default Input
