6961369de08a1ee60cf7d47d9e98c59bfa971848
Nazar <ups14q@gmail.com>
2024-11-03 11:37:48 +0200

    ch2

--- src/components/ProductsGrid/ProductsGrid.tsx
+++ src/components/ProductsGrid/ProductsGrid.tsx
@@ -5,7 +5,7 @@ import Product from '@/shared/product/Product'
 import s from './ProductsGrid.module.scss'
 
 import img1 from '@/assets/images/hoodie_3_front.webp'
-import img2 from '../../assets/images/T_7_front.webp'
+import img2 from '@/assets/images/T_7_front.webp'
 
 const ProductsGrid = () => {
   return (
